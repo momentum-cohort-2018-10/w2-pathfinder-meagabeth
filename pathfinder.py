337009b85141cf5f6ff46d42ebc97f656681025c
Mo Glatz <meagabeth@icloud.com>
2018-11-12 14:13:05 -0500

    Have starting point pixel showing up

--- pathfinder.py
+++ pathfinder.py
@@ -1,7 +1,7 @@
-# import csv
+import csv
 import random
-# import pdb
-# from PIL import Image
+import pdb
+from PIL import Image
 
 
 with open("elevation_small.txt", 'r') as f:
@@ -57,81 +57,156 @@ def print_something(list):
     for y, row in enumerate(list):
         for x, value, in enumerate(row):
             map_image.putpixel((x, y), (value, value, value))
-    map_image.save("map.png")
-    map_image.show("map.png")
+    # map_image.save("map.png")
+    # map_image.show("map.png")
+    return map_image.save("map.png")
 
 #the next line puts a single blue pixel at index (300,300), saves image and then shows image
-    map_image.putpixel((300, 300), (0, 0, 200))
-    map_image.save("map_dot.png")
-    map_image.show("map_dot.png")
+#     map_image.putpixel((300, 300), (0, 0, 200))
+#     map_image.save("map_dot.png")
+#     map_image.show("map_dot.png")
+#     return map_image
 
-# the next lines put a straight blue line vertically down the middle of the image
-    for x, row in (enumerate(list)):
-        map_image.putpixel((x, 300), (0, 0, 200))
-    map_image.save("map_vertical_line.png")
-    map_image.show("map_vertical_line.png")
-
-# print_something(intensity_list)
+# # the next lines put a straight blue line vertically down the middle of the image
+#     for x, row in (enumerate(list)):
+#         map_image.putpixel((x, 300), (0, 0, 200))
+#     map_image.save("map_vertical_line.png")
+#     map_image.show("map_vertical_line.png")
+#     return map_image
+print_something(intensity_list)
 
 def starting_point():
     y = random.randint(0, 600)
     x = 0
-    return (x, y)
-# print(starting_point())
+    map_image = Image.open('map.png')
+    map_image.putpixel((x, y), (0, 255, 0))
+    map_image.save('map.png')
+    map_image.show('map.png')
+    map_image.close('map.png')
 
-def next_step_options(row, elevation):
-    """
-    Taking the current location and returning the location & value for possible options (fwd, up; fwd; fwd, dwn) for the next step.
-    """
-    # y = [row for row in elevation_list]
-    # x = [elevation for elevation in y]
-    (x, y) = starting_point()
-    print(x, y)
-    step_options = {}
-    
-    if y == 0:
-        step_options['straight'] = (x+1, y)
-        step_options['down'] = (x+1, y+1)
-    if y == 599:
-        step_options['straight'] = (x+1, y)
-        step_options['up'] = (x+1, y-1)
-    else:
-        step_options['straight'] = (x+1, y)
-        step_options['up'] = (x+1, y-1)
-        step_options['down'] = (x+1, y+1) 
-    # print(step_options[1])
-    # print(step_options.get('straight'))
-    
-    for k in step_options.keys():
-        print(k, step_options[k])
-        
 
-next_step_options(2, 1)
-print(elevation_list[1][9])
 
-# def get_elevation_at_index():
+
+# def next_steps(list):
+#     x = 0
+#     y = 300
+
+#     while x <= 600:
+#         start_point = elevation_list[x][y]
+#         # print(start_point)
+#         # print("test")
+# #   current_place = elevation_list[x][y]
+
+#         up = elevation_list[x+1][y-1]
+#         # print("UP: " + str(up))
+#         fwd = elevation_list[x+1][y]
+#         # print("FWD: " + str(fwd))
+#         dwn = elevation_list[x+1][y+1]
+#         # print("DWN: " + str(dwn))
+
+#         dif_up = abs(up - start_point)  
+#         # print(dif_up)
+#         dif_fwd = abs(fwd - start_point)
+#         # print(dif_fwd)
+#         dif_down = abs(dwn - start_point)
+#         # print(dif_down)
+
+
+#         # if dif_up < dif_fwd and dif_up < dif_down:
+#     #   current_place = up
+#         x += 1
+#         y -= 1
+#         print(x, y)
+#         map_image.putpixel((x, y), (0, 255, 0))
+#         map_image.save("pathfinder.png")
+#         map_image.show("pathfinder.png")
+            
+
+# #         elif dif_up < dif_fwd and dif_up == dif_down:
+# #     #   current_place = up
+# #             x += 1
+# #             y -= 1
+# #             # print(x, y)
+# #             map_image.putpixel((x, y), (0, 255, 0))
+# #             map_image.save("pathfinder.png")
+# #             # map_image.show("pathfinder.png")
+# #             continue
+
+# #         elif dif_up > dif_fwd and dif_fwd < dif_down:
+# #     #   current_place = fwd
+# #             x += 1
+# #             # print(x, y)
+# #             map_image.putpixel((x, y), (0, 255, 0))
+# #             # map_image.save("pathfinder.png")
+# #             # map_image.show("pathfinder.png")
+# #             continue
+
+# #         elif dif_up > dif_fwd and dif_fwd == dif_down:
+# #     #   current_place = fwd
+# #             x += 1
+# #             # print(x, y)
+# #             map_image.putpixel((x, y), (0, 255, 0))
+# #             # map_image.save("pathfinder.png")
+# #             # map_image.show("pathfinder.png")
+# #             continue
+
+# #         elif dif_up == dif_fwd and dif_fwd < dif_down:
+# #     #   current_place = fwd
+# #             x += 1
+# #             # print(x, y)
+# #             map_image.putpixel((x, y), (0, 255, 0))
+# #             # map_image.save("pathfinder.png")
+# #             # map_image.show("pathfinder.png")
+# #             continue
+
+# #         elif dif_up == dif_fwd and dif_fwd == dif_down:
+# #     #   current_place = fwd
+# #             x += 1
+# #             # print(x, y)
+# #             map_image.putpixel((x, y), (0, 255, 0))
+# #             # map_image.save("pathfinder.png")
+# #             # map_image.show("pathfinder.png")
+# #             continue
+
+# #         elif dif_up > dif_down and dif_fwd > dif_down:
+# #     #   current_place = dwn
+# #             x += 1
+# #             y += 1
+# #             # print(x, y)
+# #             map_image.putpixel((x, y), (0, 255, 0))
+# #             # map_image.save("pathfinder.png")
+# #             # map_image.show("pathfinder.png")
+# #             continue
+         
+# #     map_image.save("pathfinder.png")
+# #     map_image.show("pathfinder.png")
+# next_steps(elevation_list)
+
+
+
+# # # def get_elevation_at_index():
 
  
-# def step_up(coordinates):
-#     for y in elevation_list:
-#         for x in y:
-#             current_step = elevation_list[0][0]
-#             step_up = (current_step[x+1][y-1])
-#             return step_up
+# # # def step_up(coordinates):
+# # #     for y in elevation_list:
+# # #         for x in y:
+# # #             current_step = elevation_list[0][0]
+# # #             step_up = (current_step[x+1][y-1])
+# # #             return step_up
 
-# step_up(elevation_list[2][5])
+# # # step_up(elevation_list[2][5])
 
-#     # if y is the top row, the options would be move fwd or dwn
-#     if y == 0:
-#         step_forward = ([x+1],[y])
-#         step_down = elevation_list([x+1],[y+1])
-#     # if y is bottom row, the options would be move fwd or up
-#     elif y == (len(elevation_list) -1):
-#         step_forward = ([x+1],[y])
-#         step_up = ([x+1],[y-1])
-#     # if y is not top or bottom row, options are move up, fwd or dwn
-#     else:
-#         step_forward = ([x+1],[y])
-#         step_up = ([x+1],[y-1])  
-#         step_down = ([])    
-#         pass
+# # #     # if y is the top row, the options would be move fwd or dwn
+# # #     if y == 0:
+# # #         step_forward = ([x+1],[y])
+# # #         step_down = elevation_list([x+1],[y+1])
+# # #     # if y is bottom row, the options would be move fwd or up
+# # #     elif y == (len(elevation_list) -1):
+# # #         step_forward = ([x+1],[y])
+# # #         step_up = ([x+1],[y-1])
+# # #     # if y is not top or bottom row, options are move up, fwd or dwn
+# # #     else:
+# # #         step_forward = ([x+1],[y])
+# # #         step_up = ([x+1],[y-1])  
+# # #         step_down = ([])    
+# # #         pass
