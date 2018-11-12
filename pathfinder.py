9f58d6aa03bd5f01de64d0c192b9b2dc2494b8eb
Mo Glatz <meagabeth@icloud.com>
2018-11-12 15:27:45 -0500

    PIL error

--- pathfinder.py
+++ pathfinder.py
@@ -1,7 +1,8 @@
+from PIL import Image
 import csv
 import random
 import pdb
-from PIL import Image
+
 
 
 with open("elevation_small.txt", 'r') as f:
@@ -57,9 +58,14 @@ def print_something(list):
     for y, row in enumerate(list):
         for x, value, in enumerate(row):
             map_image.putpixel((x, y), (value, value, value))
-    # map_image.save("map.png")
-    # map_image.show("map.png")
+    map_image.save("map.png")
+    map_image.show("map.png")
     return map_image.save("map.png")
+
+the_map = print_something(intensity_list)
+print("now just print variable 'the_map'")
+Image.open("map.png")
+Image._show("map.png")
 
 #the next line puts a single blue pixel at index (300,300), saves image and then shows image
 #     map_image.putpixel((300, 300), (0, 0, 200))
@@ -73,114 +79,105 @@ def print_something(list):
 #     map_image.save("map_vertical_line.png")
 #     map_image.show("map_vertical_line.png")
 #     return map_image
-print_something(intensity_list)
+# print_something(intensity_list)
 
-def starting_point():
-    y = random.randint(0, 600)
-    x = 0
+def draw_point(x, y):
+    """
+    Theoretically, this should draw a point on the map based on it's given (x, y) parameters.
+    """
     map_image = Image.open('map.png')
     map_image.putpixel((x, y), (0, 255, 0))
     map_image.save('map.png')
-    map_image.show('map.png')
-    map_image.close('map.png')
+    # map_image.show('map.png')
+    map_image.close()
+
+def where_to_start():
+    y = random.randint(0, 600)
+    x = 0
+    return (x, y)
 
 
+def next_steps(list):
+    x = 0
+    y = 300
+
+    while x < 599 and y < 599:
+        start_point = elevation_list[x][y]
+        # print(start_point)
+        # print("test")
+#   current_place = elevation_list[x][y]
+
+        up = elevation_list[x+1][y-1]
+        # print("UP: " + str(up))
+        fwd = elevation_list[x+1][y]
+        # print("FWD: " + str(fwd))
+        dwn = elevation_list[x+1][y+1]
+        # print("DWN: " + str(dwn))
+
+        dif_up = abs(up - start_point)  
+        # print(dif_up)
+        dif_fwd = abs(fwd - start_point)
+        # print(dif_fwd)
+        dif_down = abs(dwn - start_point)
+        # print(dif_down)
 
 
-# def next_steps(list):
-#     x = 0
-#     y = 300
-
-#     while x <= 600:
-#         start_point = elevation_list[x][y]
-#         # print(start_point)
-#         # print("test")
-# #   current_place = elevation_list[x][y]
-
-#         up = elevation_list[x+1][y-1]
-#         # print("UP: " + str(up))
-#         fwd = elevation_list[x+1][y]
-#         # print("FWD: " + str(fwd))
-#         dwn = elevation_list[x+1][y+1]
-#         # print("DWN: " + str(dwn))
-
-#         dif_up = abs(up - start_point)  
-#         # print(dif_up)
-#         dif_fwd = abs(fwd - start_point)
-#         # print(dif_fwd)
-#         dif_down = abs(dwn - start_point)
-#         # print(dif_down)
-
-
-#         # if dif_up < dif_fwd and dif_up < dif_down:
-#     #   current_place = up
-#         x += 1
-#         y -= 1
-#         print(x, y)
-#         map_image.putpixel((x, y), (0, 255, 0))
-#         map_image.save("pathfinder.png")
-#         map_image.show("pathfinder.png")
+        if dif_up < dif_fwd and dif_up < dif_down:
+        # current_place = up
+            x += 1
+            y -= 1
+            draw_point(x, y)
+            continue
             
+        elif dif_up < dif_fwd and dif_up == dif_down:
+    #   current_place = up
+            x += 1
+            y -= 1
+            draw_point(x, y)
+            continue
 
-# #         elif dif_up < dif_fwd and dif_up == dif_down:
-# #     #   current_place = up
-# #             x += 1
-# #             y -= 1
-# #             # print(x, y)
-# #             map_image.putpixel((x, y), (0, 255, 0))
-# #             map_image.save("pathfinder.png")
-# #             # map_image.show("pathfinder.png")
-# #             continue
+        elif dif_up > dif_fwd and dif_fwd < dif_down:
+    #   current_place = fwd
+            x += 1
+            # print(x, y)
+            draw_point(x, y)
+            continue
 
-# #         elif dif_up > dif_fwd and dif_fwd < dif_down:
-# #     #   current_place = fwd
-# #             x += 1
-# #             # print(x, y)
-# #             map_image.putpixel((x, y), (0, 255, 0))
-# #             # map_image.save("pathfinder.png")
-# #             # map_image.show("pathfinder.png")
-# #             continue
+        elif dif_up > dif_fwd and dif_fwd == dif_down:
+    #   current_place = fwd
+            x += 1
+            # print(x, y)
+            draw_point(x, y)
+            continue
 
-# #         elif dif_up > dif_fwd and dif_fwd == dif_down:
-# #     #   current_place = fwd
-# #             x += 1
-# #             # print(x, y)
-# #             map_image.putpixel((x, y), (0, 255, 0))
-# #             # map_image.save("pathfinder.png")
-# #             # map_image.show("pathfinder.png")
-# #             continue
+        elif dif_up == dif_fwd and dif_fwd < dif_down:
+    #   current_place = fwd
+            x += 1
+            # print(x, y)
+            draw_point(x, y)
 
-# #         elif dif_up == dif_fwd and dif_fwd < dif_down:
-# #     #   current_place = fwd
-# #             x += 1
-# #             # print(x, y)
-# #             map_image.putpixel((x, y), (0, 255, 0))
-# #             # map_image.save("pathfinder.png")
-# #             # map_image.show("pathfinder.png")
-# #             continue
+        elif dif_up == dif_fwd and dif_fwd == dif_down:
+    #   current_place = fwd
+            x += 1
+            # print(x, y)   
+            draw_point(x, y)    
+            continue
 
-# #         elif dif_up == dif_fwd and dif_fwd == dif_down:
-# #     #   current_place = fwd
-# #             x += 1
-# #             # print(x, y)
-# #             map_image.putpixel((x, y), (0, 255, 0))
-# #             # map_image.save("pathfinder.png")
-# #             # map_image.show("pathfinder.png")
-# #             continue
-
-# #         elif dif_up > dif_down and dif_fwd > dif_down:
-# #     #   current_place = dwn
-# #             x += 1
-# #             y += 1
-# #             # print(x, y)
-# #             map_image.putpixel((x, y), (0, 255, 0))
-# #             # map_image.save("pathfinder.png")
-# #             # map_image.show("pathfinder.png")
-# #             continue
-         
+        elif dif_up > dif_down and dif_fwd > dif_down:
+    #   current_place = dwn
+            x += 1
+            y += 1
+            # print(x, y)
+            draw_point(x, y)
+            continue
+    # else:
+    #     return    
 # #     map_image.save("pathfinder.png")
-# #     map_image.show("pathfinder.png")
-# next_steps(elevation_list)
+        
+next_steps(elevation_list)
+print("did we get to the end?")
+pathfinder = the_map.save('map.png')
+pathfinder = the_map.show('map.png')
 
 
 
